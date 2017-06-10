db710b8b46700db4c475bfd6e51db97dbedcb84d
Owen Essen <owenessen@Owens-MacBook-Air.local>
2017-06-10 00:17:18 -0400

    Continuing to try to get seed numbers figured out.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,21 +4,39 @@
 // This loop runs until the if statement gets the conditions it needs and breaks the loop.
 while ( true ) {
 	// This section generates a lot of random numbers as possible values for the various characters. Organizationally the idea is lower characters are traditionally less powerful Star Wars characters. IE-- Jar Jar shouldn't be stroger than Yoda. 
-	OneHealth = Math.floor((Math.random() * 100) + 1);
-	OneBaseAt = Math.floor((Math.random() * 10) + 1);
-	OneCountAt = Math.floor((Math.random() * 30) + 1);
+//	OneHealth = Math.floor((Math.random() * 100) + 1);
+//	OneBaseAt = Math.floor((Math.random() * 10) + 1);
+//	OneCountAt = Math.floor((Math.random() * 30) + 1);
 
-	TwoHealth = Math.floor((Math.random() * 100) + 1);
-	TwoBaseAt = Math.floor((Math.random() * 10) + 1);
-	TwoCountAt = Math.floor((Math.random() * 30) + 1);
+//	TwoHealth = Math.floor((Math.random() * 100) + 1);
+//	TwoBaseAt = Math.floor((Math.random() * 10) + 1);
+//	TwoCountAt = Math.floor((Math.random() * 30) + 1);
 
-	ThreeHealth = Math.floor((Math.random() * 125) + 1);
-	ThreeBaseAt = Math.floor((Math.random() * 10) + 1);
-	ThreeCountAt = Math.floor((Math.random() * 30) + 1);
+//	ThreeHealth = Math.floor((Math.random() * 125) + 1);
+//	ThreeBaseAt = Math.floor((Math.random() * 10) + 1);
+//	ThreeCountAt = Math.floor((Math.random() * 30) + 1);
 
-	FourHealth = Math.floor((Math.random() * 125) + 1);
-	FourBaseAt = Math.floor((Math.random() * 10) + 1);
-	FourCountAt = Math.floor((Math.random() * 30) + 1);
+//	FourHealth = Math.floor((Math.random() * 125) + 1);
+//	FourBaseAt = Math.floor((Math.random() * 10) + 1);
+//	FourCountAt = Math.floor((Math.random() * 30) + 1);
+
+
+	OneHealth = 50 //prompt("Jar Jar's health.");
+	OneBaseAt = 2 //prompt("Jar Jar's base attack.");
+	OneCountAt = prompt("Jar Jar's counter attack.");
+
+	TwoHealth = 60 //prompt("Boba Fett's health.");
+	TwoBaseAt = 3 //prompt("Boba Fett's base attack.");
+	TwoCountAt = prompt("Boba Fett's counter attack.");
+
+	ThreeHealth = 70 //prompt("Anakin's health.");
+	ThreeBaseAt = 4 //prompt("Anakin's base attack.");
+	ThreeCountAt = prompt("Anakin's counter attack.");
+
+	FourHealth = 80 //prompt("Yoda's health.");
+	FourBaseAt = 5 //prompt("Yoda's base attack.");
+//	FourCountAt = prompt("Yoda's count attack.");
+
 
 	// This simulates game plays in order to test if the above values will work in the context of the game.
 		var OneTestHealth = OneHealth;
@@ -112,7 +130,9 @@ while ( true ) {
 			var seedValues = [OneHealth, OneBaseAt, OneCountAt, TwoHealth, TwoBaseAt, TwoCountAt, ThreeHealth, ThreeBaseAt, ThreeCountAt, FourHealth, FourBaseAt, FourCountAt];
 			console.log(seedValues);
 			break;
-	}
+	} //else {
+		//alert("Those values don't work.");
+	//}
 }
 alert("Not in for loop.");
 
