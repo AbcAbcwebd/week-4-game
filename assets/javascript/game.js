84756959e416e1664361ade6fbe43e628155611d
Owen Essen <owenessen@Owens-MacBook-Air.local>
2017-06-09 22:18:08 -0400

    Successfully created while if loop to run until conditons met.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,52 @@
 // Functionality to dynamically generate health scores, base attack scores and counter attack scores based on the specifications of this game. 
 // Generating these values dynamically will ensure that every game is slightly different. 
-var number = 0;
-for (
-	var i = 0;
-	// This checks to ensure value ratios that must be true in order for the game to work properly.
-	number < 101;
-	i++
-	) {
-	number = Math.floor((Math.random() * 150) + 50);
+
+// This loop runs until the if statement gets the conditions it needs and breaks the loop.
+while ( true ) {
+	// This section generates a lot of random numbers as possible values for the various characters. Organizationally the idea is lower characters are traditionally less powerful Star Wars characters. IE-- Jar Jar shouldn't be stroger than Yoda. 
+	OneHealth = Math.floor((Math.random() * 100) + 1);
+	OneBaseAt = Math.floor((Math.random() * 10) + 1);
+	OneCountAt = Math.floor((Math.random() * 30) + 1);
+
+	TwoHealth = Math.floor((Math.random() * 100) + 1);
+	TwoBaseAt = Math.floor((Math.random() * 10) + 1);
+	TwoCountAt = Math.floor((Math.random() * 30) + 1);
+
+	ThreeHealth = Math.floor((Math.random() * 125) + 1);
+	ThreeBaseAt = Math.floor((Math.random() * 10) + 1);
+	ThreeCountAt = Math.floor((Math.random() * 30) + 1);
+
+	FourHealth = Math.floor((Math.random() * 125) + 1);
+	FourBaseAt = Math.floor((Math.random() * 10) + 1);
+	FourCountAt = Math.floor((Math.random() * 30) + 1);
+	if (
+		//This part checks to see if the numbers generated will work for the game. 
+
+		//Characters one and two must be weaker than characters three and four. 
+		OneHealth < ThreeHealth &&
+		OneHealth < FourHealth &&
+		OneBaseAt < ThreeBaseAt &&
+		OneBaseAt < FourBaseAt &&
+		OneCountAt < ThreeCountAt &&
+		OneCountAt < FourCountAt &&
+
+		TwoHealth < ThreeHealth &&
+		TwoHealth < FourHealth &&
+		TwoBaseAt < ThreeBaseAt &&
+		TwoBaseAt < FourBaseAt &&
+		TwoCountAt < ThreeCountAt &&
+		TwoCountAt < FourCountAt &&
+
+		//Weaker character must be able to win. 
+
+		//Strongest character must be able to lose.
+		) {
+		//Functionality that runs if above conditions are met.
+		alert("Conditions met.");
+		break;
+	}
 }
-alert(number);
+alert("Not in for loop.");
 
 // This object constructor defines the format by which character information will be organized.
 function character(name, healthPoints, attackPower, counterAttackPower, imageSource) {
@@ -21,4 +58,3 @@ function character(name, healthPoints, attackPower, counterAttackPower, imageSou
 }
 
 var jarJar = new character("Jar Jar", 50, 5, );
-var myMother = new person("Sally", "Rally", 48, "green");
