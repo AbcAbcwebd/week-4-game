731a63569236ae413faa44efec2d497d5432fa8c
Owen Essen <owenessen@Owens-MacBook-Air.local>
2017-06-10 19:16:01 -0400

    Player can now select a character.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,22 @@
 
 	// This object constructor defines the format by which character information will be organized.
-	function character(name, healthPoints, attackPower, counterAttackPower, imageSource) {
+	function character(name, healthPoints, attackPower, counterAttackPower, imageSource, ID) {
 	    this.name = name;
 	    this.healthPoints = healthPoints;
 	    this.attackPower = attackPower;
 	    this.counterAttackPower = counterAttackPower;
 	    this.imageSource = imageSource;
+	    this.ID = ID;
 	}
 
 	// The characters 
 	// I had difficulty coming up with numbers met the qualifications of the game. IE-- Weakest character could always win, strongest could always lose, etc. After spending many hours trying to develop and automated way to seed numbers to the game, I instead borrowed numbers from a similar game I found online. I figured it was more important to move on to the actual coding. 
-	var mace = new character("Mace Windu", 160, 10, 20, "mace.png");
-	var boba = new character("Boba Fett", 130, 15, 30, "boba.jpg");
-	var anakin = new character("Anakin", 180, 7, 15, "anakin.jpg");
-	var yoda = new character("Yoda", 180, 15, 25, "yoda.jpg");
-	var R2D2 = new character("R2D2", 110, 12, 20, "r2d2.png");
-	var jarJar = new character("Jar Jar", 100, 12, 24, "jar-jar.jpg");
+	var mace = new character("Mace Windu", 160, 10, 20, "mace.png", "mace");
+	var boba = new character("Boba Fett", 130, 15, 30, "boba.jpg", "boba");
+	var anakin = new character("Anakin", 180, 7, 15, "anakin.jpg", "anakin");
+	var yoda = new character("Yoda", 180, 15, 25, "yoda.jpg", "yoda");
+	var R2D2 = new character("R2D2", 110, 12, 20, "r2d2.png", "r2d2");
+	var jarJar = new character("Jar Jar", 100, 12, 24, "jar-jar.jpg", "jar-jar");
 
 	// Other global variables
 	var characterArray = [mace, boba, anakin, yoda, R2D2, jarJar];
@@ -26,7 +27,7 @@
 
 	// This function creates character tiles to add to the various sections. 
 	function generateTile(character_name) {
-		characterTile = '<div class="character-tile"><p>' + character_name.name + '</p><br><img src="assets/images/' + character_name.imageSource + '" class="character-image"><p>' + character_name.healthPoints + '</p></div>';
+		characterTile = '<div class="character-tile" id="' + character_name.ID + 'Tile"><p>' + character_name.name + '</p><br><img src="assets/images/' + character_name.imageSource + '" class="character-image"><p>' + character_name.healthPoints + '</p></div>';
 		return characterTile;
 	}
 
@@ -41,12 +42,12 @@
 
 $(document).ready(function() {
 
-	console.log($(".character-tile"));
+
 	// Click functionality for selecting your player
 	$(".character-tile").click(function(){
-	  alert("You clicked a character!");
 	  if (yourCharacter === null) {
-	  	yourCharacter = this.value;
+	  	yourCharacter = this.id;
+	  	$( "#possible-characters" ).empty();
 	  }
 	});
 
